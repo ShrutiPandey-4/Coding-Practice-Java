6c97fcc73aa982eba3b3fd912f67beabe18d29dd
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-17 23:48:42 +0530

    "Added few more practice problems"

---
+++ src/AsciiValue.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AsciiValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0); // take first character input
+
+        int ascii = (int) ch; // typecast char to int
+
+        System.out.println("ASCII value of '" + ch + "' is: " + ascii);
+    }
+}
+
