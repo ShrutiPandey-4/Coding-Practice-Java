7de649717269969cf758fe2f85456183246b8fb0
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-18 21:12:13 +0530

    "Added more new practice problems again"

---
+++ src/AddIntDouble.java
@@ -0,0 +1,9 @@
+public class AddIntDouble {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 6.5;
+        double result = a + b;  // int automatically promoted to double
+
+        System.out.println("Sum of int and double = " + result);
+    }
+}
