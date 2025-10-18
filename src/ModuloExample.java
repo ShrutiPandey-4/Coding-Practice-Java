57b5a990e0610e58ae1974c690bda077eae67a68
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-18 21:18:52 +0530

    "Added more new practice problems again"

---
+++ src/ModuloExample.java
@@ -0,0 +1,9 @@
+public class ModuloExample {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 5;
+        int remainder = a % b;
+
+        System.out.println("Remainder of " + a + " divided by " + b + " = " + remainder);
+    }
+}
