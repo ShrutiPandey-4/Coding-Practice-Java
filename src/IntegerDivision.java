78ffd1fb7b4ed650771b33dcbe130bdc7d55d588
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-18 21:18:25 +0530

    "Added more new practice problems again"

---
+++ src/IntegerDivision.java
@@ -0,0 +1,9 @@
+public class IntegerDivision {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 2;
+        int result = a / b;  // integer division
+
+        System.out.println("Result of " + a + " / " + b + " = " + result);
+    }
+}
