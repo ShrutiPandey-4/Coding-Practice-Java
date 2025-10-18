1e84d616925e15979ec80f827652300ea91bdad5
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-18 21:11:18 +0530

    "Added more new practice problems again"

---
+++ src/ExplicitCasting.java
@@ -0,0 +1,9 @@
+public class ExplicitCasting {
+    public static void main(String[] args) {
+        double d = 9.78;
+        int i = (int) d;   // explicit casting from double → int
+
+        System.out.println("Double value: " + d);
+        System.out.println("After explicit casting to int: " + i);
+    }
+}
