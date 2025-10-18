b7cd16a1ddeedc1f0b44fcd87ec740103ae87c9c
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-18 21:12:55 +0530

    "Added more new practice problems again"

---
+++ src/AddCharInt.java
@@ -0,0 +1,11 @@
+public class AddCharInt {
+    public static void main(String[] args) {
+        char ch = 'A';  // ASCII value of 'A' = 65
+        int num = 5;
+        int result = ch + num;
+
+        System.out.println("Character: " + ch);
+        System.out.println("Integer: " + num);
+        System.out.println("Result (char + int): " + result);
+    }
+}
