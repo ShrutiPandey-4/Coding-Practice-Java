083902c6551f7eb833abdc2041fed60bb4941df0
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-21 23:54:01 +0530

    "adding more solved problems for java today"

---
+++ src/EvenOddIndexSum.java
@@ -0,0 +1,16 @@
+public class EvenOddIndexSum {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int evenSum = 0, oddSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0)
+                evenSum += arr[i];
+            else
+                oddSum += arr[i];
+        }
+
+        System.out.println("Sum at even indices: " + evenSum);
+        System.out.println("Sum at odd indices: " + oddSum);
+    }
+}
