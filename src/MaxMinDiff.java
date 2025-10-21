dffbdd064a7cb7164ac45137ac834594d0299ecf
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-21 23:54:16 +0530

    "adding more solved problems for java today"

---
+++ src/MaxMinDiff.java
@@ -0,0 +1,13 @@
+public class MaxMinDiff {
+    public static void main(String[] args) {
+        int[] arr = {3, 7, 2, 9, 5};
+        int max = arr[0], min = arr[0];
+
+        for (int n : arr) {
+            if (n > max) max = n;
+            if (n < min) min = n;
+        }
+
+        System.out.println("Difference = " + (max - min));
+    }
+}
