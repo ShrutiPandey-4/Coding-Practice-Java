34dc1376d66be1f98a3195ae1b0d2b0efe1f302a
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-22 20:00:08 +0530

    "Numbers"

---
+++ src/Logic_Building/Num1to10.java
@@ -0,0 +1,10 @@
+package Logic_Building;
+
+public class Num1to10 {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1; i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
