a7a3697d9de38bbf1fe77e993999558015887d99
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-25 21:59:01 +0530

    Prime Check

---
+++ src/Logic_Building/PrimeCheck.java
@@ -0,0 +1,28 @@
+package Logic_Building;
+
+import java.util.Scanner;
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        if(num <=1 ){
+            System.out.println(num+" is not prime");
+            return;
+        }
+        boolean isPrime = true;
+
+        for(int i =2;i<= Math.sqrt(num);i++){
+            if(num % i ==0 ){
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime)
+            System.out.println(num+" is prime");
+        else
+            System.out.println(num+" is not prime");
+    }
+}
