d64dbafe0bd2302503b1a8e2e3fa2b342d06e6fd
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-27 23:46:54 +0530

    Check for Armstrong number

---
+++ src/Function_Methods/ArmstrongCheck.java
@@ -0,0 +1,24 @@
+package Function_Methods;
+
+import java.util.Scanner;
+
+public class ArmstrongCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int temp = n, sum = 0;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+
+        if (sum == n)
+            System.out.println(n + " is an Armstrong number.");
+        else
+            System.out.println(n + " is not an Armstrong number.");
+    }
+}
+
