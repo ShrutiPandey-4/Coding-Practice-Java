a3e6592f57468e6d0a34a63a55bbfc9ddacd4917
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-29 18:47:31 +0530

    Copy an array

---
+++ src/Practice/CopyArray.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = new int[arr1.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[i];
+        }
+
+        System.out.println("Copied Array:");
+        for (int n : arr2) {
+            System.out.print(n + " ");
+        }
+    }
+}
+
