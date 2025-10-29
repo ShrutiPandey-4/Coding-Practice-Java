6affaec803fa7b428cc7042b2651afd416280aa5
ShrutiPandey-4 <23053394@kiit.ac.in>
2025-10-29 18:50:21 +0530

    Smallest element in an array

---
+++ src/Practice/SmallestElement.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class SmallestElement {
+    public static void main(String[] args) {
+        int[] arr = {4, 9, 1, 7};
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min)
+                min = arr[i];
+        }
+
+        System.out.println("Smallest = " + min);
+    }
+}
+
